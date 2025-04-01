5824e3dd08f9c87a51cd5c2cf7b1b78d3b71148e
lhoda <lhodriscoll@wpi.edu>
2025-04-01 17:06:09 -0400

    component setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import Title from "./components/title.tsx";
+import Intro from "./components/intro.tsx";
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-    <Title/>
-    </>
+      <>
+        <Title/>
+        <Intro name1={"Liam"} name2={"Camden"}>
+
+        </Intro>
+      </>
   )
 }
 
 export default App
+
+//Testing
